a618dd4a283813ee52cceb407d8489304c0d76f8
JhoelFernandoIM <i2512152@continental.edu.pe>
2025-12-14 01:49:55 -0500

    prueba2

--- db/conexion.py
+++ db/conexion.py
@@ -1,11 +1,11 @@
 import psycopg2
+import streamlit as st
 
 def conectar_db():
-    conexion = psycopg2.connect(
-        host="db.nulrfxommxyhpgvsweri.supabase.co",
-        database="postgres",
-        user="postgres",
-        password="FREEdos",
-        port="5432"
+    return psycopg2.connect(
+        host=st.secrets["DB_HOST"],
+        database=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        port=st.secrets["DB_PORT"]
     )
-    return conexion
